e468746441fd0c1fe001115f44c28238cbb87737
Ed Pascoe <ed@dns.business>
2022-08-01 15:42:10 +0200

    Dedicated functions for doing prometheus tests

---
+++ cmd/eppprom/main.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2022 Ed Pascoe.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"epptester/eppprom"
+	_ "epptester/pkg/log"
+	"flag"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+var Version = "dev"
+var GitTag = "-"
+
+// The flag help message.
+func Usage() {
+	w := flag.CommandLine.Output() // may be os.Stderr - but not necessarily
+	fmt.Fprintln(w, `eppprom
+Testing remote EPP connections.
+See 
+`)
+	flag.PrintDefaults()
+}
+
+func main() {
+	logrus.Debugln("Start")
+	configfile := flag.String("C", "eppprom.yanl", "Yaml config file")
+	version := flag.Bool("version", false, "Show version")
+	flag.Usage = Usage
+	flag.Parse()
+	if *version {
+		fmt.Println("epptester", Version)
+		fmt.Println("Git version.\t", GitTag)
+		os.Exit(0)
+	}
+	// epptester.RunTest(*certFile, *keyFile, *host, *port, *username, *password, *tlsversion)
+	config := &eppprom.Config{}
+	eppprom.Loadconfig(*configfile, config)
+	eppprom.Testzones(config)
+
+}
